e5da7086c86bc7da6456587882ebd8da835e2598
wetoook <50036016+wetoook@users.noreply.github.com>
2023-12-04 16:35:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,12 +10,12 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'FPIW.COM',
+  HEO_HERO_TITLE_1: '与你分享',
+  HEO_HERO_TITLE_2: '让世界更开阔',
+  HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '美股2024年会如何？',
-  HEO_HERO_TITLE_LINK: 'baidu.COM',
+  HEO_HERO_TITLE_LINK: 'sq',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
