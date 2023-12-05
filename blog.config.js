c9f5746d6f384cccb4efee5686a2f3d84fc39e11
wetoook <50036016+wetoook@users.noreply.github.com>
2023-12-05 09:28:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Sovex', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Suruy', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '与你分享', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://fpiw.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '技术,知识,股票,美股,港股,A股,育儿，宝宝,儿童,', // 网站关键词 英文逗号隔开
