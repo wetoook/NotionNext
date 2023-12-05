694c748af73033d10c0f18e559c3eb0940f3c066
wetoook <50036016+wetoook@users.noreply.github.com>
2023-12-05 10:10:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: ' ',
   HEO_HERO_TITLE_4: '未来展望',
   HEO_HERO_TITLE_5: '美股2024年会如何？',
-  HEO_HERO_TITLE_LINK: 'https://www.fpiw.com/article/821ee92d-98bf-4867-90f6-94348e8c9667',
+  HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
